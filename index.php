3d9faae257b1abe575e1ad429e5815ec16fa4b7d
Your Name <abhijitstomar@gmail.com>
2014-11-26 18:55:19 +0530

    bootstrapped

--- index.php
+++ index.php
@@ -4,10 +4,82 @@
         <title>
             YouTube Song Request
         </title>
+        <link href='https://fonts.googleapis.com/css?family=Roboto|Oswald|Open Sans|Montserrat' rel='stylesheet' type='text/css'>
+        <style type="text/css">
+        body{
+            background:
+            linear-gradient(45deg, #a1e877 45px, transparent 45px)64px 64px,
+            linear-gradient(45deg, #a1e877 45px, transparent 45px,transparent 91px, #f5f4f7 91px, #f5f4f7 135px, transparent 135px),
+            linear-gradient(-45deg, #a1e877 23px, transparent 23px, transparent 68px,#a1e877 68px,#a1e877 113px,transparent 113px,transparent 158px,#a1e877 158px);
+            background-color:#f5f4f7;
+            background-size: 128px 128px;
+        }
+        #timer{
+                font-family: "Monospace";
+        }
+        </style>
+        <script type="text/javascript" >
+            window.onload = function(){
+                var target_date = new Date("Dec 26, 2014").getTime();
+                var current_date = new Date().getTime();
+                var centiseconds_left = (target_date - current_date + 36000000)/10;
+                CreateTimer("timer", centiseconds_left);
+            }
+
+            var Timer;
+            var TotalcSeconds;
+
+
+            function CreateTimer(TimerID, Time) {
+            Timer = document.getElementById(TimerID);
+            TotalcSeconds = Time;
+
+            UpdateTimer()
+            window.setTimeout("Tick()", 1);
+            }
+
+            function Tick() {
+            if (TotalcSeconds <= 0) {
+            alert("Time's up!")
+            return;
+            }
+
+            TotalcSeconds -= 1;
+            UpdateTimer()
+            window.setTimeout("Tick()", 10);
+            }
+             
+
+            function UpdateTimer() {
+            var target_date = new Date("Dec 26, 2014").getTime();
+            var current_date = new Date().getTime();
+            var centiseconds_left = (target_date - current_date + 36000000)/10;
+
+            var cSeconds = TotalcSeconds;
+
+            var cSeconds=centiseconds_left;
+            var Days = Math.floor(cSeconds / 8640000);
+            cSeconds -= Days * 8640000;
+            var Hours = Math.floor(cSeconds / 360000);
+            cSeconds -= Hours * (360000);
+            var Minutes = Math.floor(cSeconds / 6000);
+            cSeconds -= Minutes * (6000);
+            var Seconds = Math.floor(cSeconds / 100);
+            cSeconds -= Seconds * (100);
+
+            var TimeStr = ((Days > 0) ? Days + " DAYS " : "") + LeadingZero(Hours) + ":" + LeadingZero(Minutes) + ":" + LeadingZero(Seconds) + "." + LeadingZero(Math.floor(cSeconds))
+            Timer.innerHTML = TimeStr;
+            }
+
+            function LeadingZero(Time) {
+            return (Time < 10) ? "0" + Time : + Time;
+            }
+        </script>
     </head>
     <body>
+    <h1 style="text-align:center; font-family:Roboto;">MI 2014 YOGA ROOM <div id='timer' /></h1>
 	<?php
-    echo "<h1>Playlist:</h1>\n\t\t<ul>";
+    echo "<h2 style='font-family:Oswald;'>PLAYLIST:</h2>\n\t\t<ul>";
     $file = ('playlist.txt');
     $fh = fopen($file, "r") or die('Could not open file!');
     $data = fread($fh, filesize($file)) or die('Could not read file!');
@@ -47,7 +119,7 @@
     $pieces = explode("\n", $data);
     $x=1;
     foreach ($pieces as $value) {
-        if ($x%5 === 3 ) echo "\n\t\t\t<li> $value";  // Echoes every third line in the file i.e. the song names
+        if ($x%5 === 3 ) echo "\n\t\t\t<li style='font-family:Open Sans;'><b> $value </b></li>";  // Echoes every third line in the file i.e. the song names
         if ($x%5 === 4 ) echo "<em> $value </em>"; //Echoes the LDAP of the person who requested the song
         $x++;
     }
@@ -57,10 +129,11 @@
     fclose($fh);
     ?>
 		<form action="" method="post">
-		Youtube URL: <input type="text" name="name"><br>
-        LDAP ID: <input type="text" name="id"><br>
-        LDAP Password: <input type="password" name="pass"><br>
-		<input type="submit">
+		<div style="font-family:Montserrat;">Youtube URL:<input style="left:30%;position:absolute;" type="text" name="name"></div>
+        <div style="font-family:Montserrat;">LDAP ID:<input style="left:30%;position:absolute;" type="text" name="id"></div>
+        <div style="font-family:Montserrat;">LDAP Password:<input style="left:30%;position:absolute;" type="password" name="pass"></div>
+		<input style="color:white; background-color: #337AB7; border-color: #2E6DA4; border:1px solid transparent; border-radius:4px; padding:5px 10px; margin-top:10px;" type="submit">
 		</form>
+        <div style="font-family:Montserrat;font-size:15px;text-align:center;">Credits- Web and Creatives</div>
 	</body>
 </html>
